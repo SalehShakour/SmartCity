17295102954a2a459e0c509d02327d3e0add2efe
Saleh Shakour <saleh.shakour2000@gmail.com>
2023-08-11 21:04:13 +0330

    Add people's general information

--- src/main/java/person/Person.java
+++ src/main/java/person/Person.java
@@ -1,5 +1,15 @@
 package person;
-
-public abstract class Person {
+import java.time.LocalDate;
+public abstract class Person  {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Gender gender;
+    private LocalDate birthday;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private final String validEmailRgx = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+    private BloodType bloodType;
 
 }
