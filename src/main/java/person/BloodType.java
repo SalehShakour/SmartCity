ee3ecf4959f047d92559f98bad3fde7eec61d2c2
Saleh Shakour <saleh.shakour2000@gmail.com>
2023-08-11 22:50:13 +0330

    Add Docstring

--- src/main/java/person/BloodType.java
+++ src/main/java/person/BloodType.java
@@ -1,5 +1,18 @@
 package person;
 
+/**
+ * Enumeration representing different blood types.
+ * <p>
+ * Blood types include:
+ * - A_positive
+ * - B_positive
+ * - O_positive
+ * - AB_positive
+ * - A_negative
+ * - B_negative
+ * - O_negative
+ * - AB_negative
+ */
 public enum BloodType {
-    A_positive,B_positive,O_positive,AB_positive,A_negative,B_negative,O_negative,AB_negative
+    A_positive, B_positive, O_positive, AB_positive, A_negative, B_negative, O_negative, AB_negative
 }
